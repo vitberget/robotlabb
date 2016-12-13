66fd354c8689dd12f292655d4d9f527bddb4c487
Kenneth <github@vitberget.se>
2016-12-13 19:38:57 +0100

    Nicer names

--- targetting_robot/targetting_robot.ino
+++ targetting_robot/targetting_robot.ino
@@ -1,56 +1,55 @@
 #define ON 1
 #define OFF 0
 
-#define LS 4
-#define LF 7
-#define LR 8
+#define LEFT_SPEED 4
+#define LEFT_FORWARD 7
+#define LEFT_REVERSE 8
 
-#define RS 3
-#define RF 5
-#define RR 6
+#define RIGHT_SPEED 3
+#define RIGHT_FORWARD 5
+#define RIGHT_REVERSE 6
 
-void setup() {                
+void setup() {
   Serial.begin(38400);
   pinMode(A0,INPUT);
   pinMode(A1,INPUT);
 
-  pinMode(LS, OUTPUT);
-  pinMode(RS, OUTPUT);
-  pinMode(LF, OUTPUT);
-  pinMode(LR, OUTPUT);
-  pinMode(RF, OUTPUT);
-  pinMode(RR, OUTPUT);
+  pinMode(LEFT_SPEED, OUTPUT);
+  pinMode(RIGHT_SPEED, OUTPUT);
+  pinMode(LEFT_FORWARD, OUTPUT);
+  pinMode(LEFT_REVERSE, OUTPUT);
+  pinMode(RIGHT_FORWARD, OUTPUT);
+  pinMode(RIGHT_REVERSE, OUTPUT);
 
-  analogWrite(LS, 200);
-  analogWrite(RS, 200);
+  analogWrite(LEFT_SPEED, 200);
+  analogWrite(RIGHT_SPEED, 200);
 }
 
 void loop() {
   int left = analogRead(A0);
   int right = analogRead(A1);
   int diff = left - right;
-  Serial.printf("[%4d %4d %4d] ",left,right,diff);
+  Serial.printf("[%4d %4d %5d] ",left,right,diff);
 
   if(diff < -100) {
     Serial.println("Turning LEFT");
-    digitalWrite(LF, OFF);
-    digitalWrite(LR, ON);
-    digitalWrite(RF, ON);
-    digitalWrite(RR, OFF);
+    digitalWrite(LEFT_FORWARD, OFF);
+    digitalWrite(LEFT_REVERSE, ON);
+    digitalWrite(RIGHT_FORWARD, ON);
+    digitalWrite(RIGHT_REVERSE, OFF);
   } else if (diff > 100) {
     Serial.println("Turning RIGHT");
-    digitalWrite(LF, ON);
-    digitalWrite(LR, OFF);
-    digitalWrite(RF, OFF);
-    digitalWrite(RR, ON);
+    digitalWrite(LEFT_FORWARD, ON);
+    digitalWrite(LEFT_REVERSE, OFF);
+    digitalWrite(RIGHT_FORWARD, OFF);
+    digitalWrite(RIGHT_REVERSE, ON);
   } else {
     Serial.println("Turning STOP");
-    digitalWrite(LF, OFF);
-    digitalWrite(LR, OFF);
-    digitalWrite(RF, OFF);
-    digitalWrite(RR, OFF);
+    digitalWrite(LEFT_FORWARD, OFF);
+    digitalWrite(LEFT_REVERSE, OFF);
+    digitalWrite(RIGHT_FORWARD, OFF);
+    digitalWrite(RIGHT_REVERSE, OFF);
   }
-  
-  delay(133);
-} 
 
+  delay(133);
+}
