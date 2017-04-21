967e39aeb4912f1509c088329db88c2f89415f2b
Karl Martinsson <karl@klyftit.se>
2017-04-21 12:52:38 +0200

    Kommandoradsbyggbar med Arduino-Makefile. Organiserade om koden så att definitioner kommer före användning.

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # robotlabb
 Sumorobot Labb
+
+
+## Utvecklingsmiljö
+
+### Arduino IDE
+
+Hämta Arduino IDE genom nedladdningslänk: https://www.arduino.cc/download_handler.php?f=/arduino-1.8.2-linux64.tar.xz.
+
+	tar xvfJ arduino-1.8.2-linux64.tar.xz
+
+	cd arduino-1.8.2
+	./install.sh
+
+### Teensy
+
+	wget https://www.pjrc.com/teensy/td_136/TeensyduinoInstall.linux64
+	wget https://www.pjrc.com/teensy/49-teensy.rules
+
+	sudo cp 49-teensy.rules /etc/udev/rules.d/
+	chmod 755 TeensyduinoInstall.linux64 
+	./TeensyduinoInstall.linux64
+
+### Möjliggör bygge från kommandoraden
+
+Från http://hardwarefun.com/tutorials/compiling-arduino-sketches-using-makefile
+
+	git clone https://github.com/sudar/Arduino-Makefile
+
+	sudo apt-get install libdevice-serialport-perl
+	sudo apt-get install libyaml-perl
+
+Lägg in i `~/.bashrc`:
+
+	export ARDUINO_DIR=`cd ~/dev/arduino-1.8.2; pwd`
+	export ARDMK_DIR=`cd ~/dev/Arduino-Makefile; pwd`
+	export AVR_TOOLS_DIR=`cd ~/dev/arduino-1.8.2/hardware/tools/avr; pwd`
+
+
+### Emacs
+
+Addera melpa arkivet:
+
+	(add-to-list 'package-archives
+		'("melpa" . "https://melpa.org/packages/"))
+
+Installera arduino-mode från melpa. 
+
+Använd normala byggkommandon (`M-x compile`) för att bygga från den Makefile som ligger i projektet.
+
--- targetting_robot/src/targetting_robot.ino
+++ targetting_robot/src/targetting_robot.ino
@@ -27,51 +27,6 @@ bool running = false;
 
 bool serial = true;
 
-void setup() {
-  if(serial) Serial.begin(115200);
-
-  pinMode(A0,INPUT);
-  pinMode(A1,INPUT);
-  pinMode(RIGHT_LINE_SENSOR,INPUT);
-  pinMode(LEFT_LINE_SENSOR,INPUT);
-
-  pinMode(RIGHT_SPEED, OUTPUT);
-  pinMode(RIGHT_FORWARD, OUTPUT);
-  pinMode(RIGHT_REVERSE, OUTPUT);
-
-  pinMode(LEFT_SPEED, OUTPUT);
-  pinMode(LEFT_FORWARD, OUTPUT);
-  pinMode(LEFT_REVERSE, OUTPUT);
-
-  pinMode(13, OUTPUT);
-
-   motors(0,0);
-
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  attachInterrupt(BUTTON_PIN, buttonPressed, FALLING);
-
-  //delay(100);
-
-  //pixels.begin();
-  //pixels.setPixelColor(0, pixels.Color(0,150,0));
-  //pixels.show();
-
-  //delay(100);
-}
-
-void buttonPressed() {
-  cli(); // Disable interrupts
-  //Serial.println("Button interupt FALLING (pressed)");
-  detachInterrupt(BUTTON_PIN);
-  if(shouldRun) {
-    motors(0,0);
-    delay(100);
-    running = false;
-  }
-  shouldRun = !shouldRun;
-  sei(); // Enable interrupts
-}
-
 void motors(int leftSpeed, int rightSpeed) {
   int l = min(255, abs(leftSpeed));
   int r = min(255, abs(rightSpeed));
@@ -111,6 +66,88 @@ void motors(int leftSpeed, int rightSpeed) {
 
 bool onOff = false;
 
+
+void buttonPressed() {
+  cli(); // Disable interrupts
+  //Serial.println("Button interupt FALLING (pressed)");
+  detachInterrupt(BUTTON_PIN);
+  if(shouldRun) {
+    motors(0,0);
+    delay(100);
+    running = false;
+  }
+  shouldRun = !shouldRun;
+  sei(); // Enable interrupts
+}
+
+int readLeft() {
+  //return analogRead(A0);
+  leftBuf = (leftBuf * LEN + analogRead(A1)) / (LEN+1);
+  return leftBuf;
+}
+
+int readRight() {
+  //return analogRead(A1);
+  rightBuf = (rightBuf * LEN + analogRead(A0)) / (LEN+1);
+  return rightBuf;
+}
+
+int readLineSensor(int sensor) {
+  return analogRead(sensor);
+}
+
+
+void rotateLeft() {
+  motors(-255,255);
+}
+
+void back() {
+  motors(-255,-255);
+}
+
+
+void turn180DegreesToEscapeWhiteLine() {
+  if (rightLineSensorCounter <= (rightLineSensorCounterStart-rightLineSensorBack)) {
+      rotateLeft();
+  } else {
+      back();
+  }
+  //if (serial) Serial.printf("COUNTER: [%4d]", rightLineSensorCounter);
+  //if (serial) Serial.println("");
+  rightLineSensorCounter--;
+}
+
+void setup() {
+  if(serial) Serial.begin(115200);
+
+  pinMode(A0,INPUT);
+  pinMode(A1,INPUT);
+  pinMode(RIGHT_LINE_SENSOR,INPUT);
+  pinMode(LEFT_LINE_SENSOR,INPUT);
+
+  pinMode(RIGHT_SPEED, OUTPUT);
+  pinMode(RIGHT_FORWARD, OUTPUT);
+  pinMode(RIGHT_REVERSE, OUTPUT);
+
+  pinMode(LEFT_SPEED, OUTPUT);
+  pinMode(LEFT_FORWARD, OUTPUT);
+  pinMode(LEFT_REVERSE, OUTPUT);
+
+  pinMode(13, OUTPUT);
+
+   motors(0,0);
+
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(BUTTON_PIN, buttonPressed, FALLING);
+
+  //delay(100);
+
+  //pixels.begin();
+  //pixels.setPixelColor(0, pixels.Color(0,150,0));
+  //pixels.show();
+
+  //delay(100);
+}
 void loop() {
   onOff = !onOff;
   digitalWrite(13, onOff ? HIGH : LOW);
@@ -185,39 +222,4 @@ void loop() {
     if (serial) Serial.println();
     delay(7);
   }
-}
-
-void turn180DegreesToEscapeWhiteLine() {
-  if (rightLineSensorCounter <= (rightLineSensorCounterStart-rightLineSensorBack)) {
-      rotateLeft();
-  } else {
-      back();
-  }
-  //if (serial) Serial.printf("COUNTER: [%4d]", rightLineSensorCounter);
-  //if (serial) Serial.println("");
-  rightLineSensorCounter--;
-}
-
-void rotateLeft() {
-  motors(-255,255);
-}
-
-void back() {
-  motors(-255,-255);
-}
-
-int readLeft() {
-  //return analogRead(A0);
-  leftBuf = (leftBuf * LEN + analogRead(A1)) / (LEN+1);
-  return leftBuf;
-}
-
-int readRight() {
-  //return analogRead(A1);
-  rightBuf = (rightBuf * LEN + analogRead(A0)) / (LEN+1);
-  return rightBuf;
-}
-
-int readLineSensor(int sensor) {
-  return analogRead(sensor);
 }
