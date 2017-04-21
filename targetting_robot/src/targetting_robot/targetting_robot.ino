bc6de5d1b121583eefb00774c095b02b0faf7305
Karl Martinsson <karl@klyftit.se>
2017-04-21 12:48:28 +0200

    Revert "Flytta senaste koden till rätt plats"
    This reverts commit b0d2cba52d5cb034c4aab5eceaa836e648cffb2d.

--- targetting_robot/src/targetting_robot/targetting_robot.ino
+++ targetting_robot/src/targetting_robot/targetting_robot.ino
@@ -9,11 +9,18 @@
 #define RIGHT_FORWARD 5
 #define RIGHT_REVERSE 6
 
+#define RIGHT_LINE_SENSOR A2
+#define LEFT_LINE_SENSOR A3
+
 #define BUTTON_PIN 9
 
 int LEN = 5;
 int leftBuf = 0;
 int rightBuf = 0;
+int rightLineSensorBack = 700; 
+int rightLineSensorCounter = 0;
+int rightLineSensorCounterStart = 3500;   //1000 = 45 grader rotation (rotationstid: rightLineSensorCounterStart-rightLineSensorBack)
+int whiteColor = 600;
 
 bool shouldRun = false;
 bool running = false;
@@ -25,6 +32,8 @@ void setup() {
 
   pinMode(A0,INPUT);
   pinMode(A1,INPUT);
+  pinMode(RIGHT_LINE_SENSOR,INPUT);
+  pinMode(LEFT_LINE_SENSOR,INPUT);
 
   pinMode(RIGHT_SPEED, OUTPUT);
   pinMode(RIGHT_FORWARD, OUTPUT);
@@ -40,6 +49,14 @@ void setup() {
 
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   attachInterrupt(BUTTON_PIN, buttonPressed, FALLING);
+
+  //delay(100);
+
+  //pixels.begin();
+  //pixels.setPixelColor(0, pixels.Color(0,150,0));
+  //pixels.show();
+
+  //delay(100);
 }
 
 void buttonPressed() {
@@ -49,12 +66,9 @@ void buttonPressed() {
   if(shouldRun) {
     motors(0,0);
     delay(100);
-    shouldRun = false;
     running = false;
-  } else {
-      shouldRun = true;
   }
-
+  shouldRun = !shouldRun;
   sei(); // Enable interrupts
 }
 
@@ -115,55 +129,95 @@ void loop() {
 
   int left = readLeft();
   int right = readRight();
+  int rightLineSensor = readLineSensor(RIGHT_LINE_SENSOR);
+  int leftLineSensor = readLineSensor(LEFT_LINE_SENSOR);
   int diff = left - right;
   int avg = (left + right) / 2;
 
-  if (serial) Serial.printf("[%4d %4d %5d] ",left,right,diff);
+ 
+  //delay(500);
+  //if (serial) Serial.printf("[%4d %4d %5d] ",left,right,diff);
+  if (serial) Serial.printf("[%4d]", leftLineSensor);
+  if (serial) Serial.printf("[%4d]", rightLineSensor);
+  //if (serial) Serial.printf("[%4d %4d %5d] ",left,right,diff);
 
-  int ll = left/32;
-  int rr = right/32;
-
-  if (serial) Serial.print("[");
-  for(int i=0; serial && i< 32; i++) {
-      Serial.print(i<=ll ? "=":"-");
+  
+  if (rightLineSensorCounter == 0 && (rightLineSensor < whiteColor)) {
+    // Found white line
+    rightLineSensorCounter = rightLineSensorCounterStart;
   }
-  if (serial) Serial.print("|");
-  for(int i=31; serial && i>-1; i--) {
-      Serial.print(i<=rr ? "=":"-");
-  }
 
-  if(serial) Serial.print("] ");
-
-  if(ll>rr+3) {
-    if (serial) Serial.print("Left");
-    motors(-255,255);
-  } else if(rr>ll+3) {
-    if (serial) Serial.print("Right");
-    motors(255,-255);
-  } else if (ll >4 && rr>4) {
-    if (serial) Serial.print("Fwd");
-    motors(255, 255);
+  if (rightLineSensorCounter > 0) {
+    turn180DegreesToEscapeWhiteLine();
   } else {
-    if (serial) Serial.print("Search");
-    if((millis()%8000)>4000) {
-      motors(255,-255);
-    } else {
-      motors(-255,255);
+    int ll = left/32;
+    int rr = right/32;
+  
+    if (serial) Serial.print("[");
+    for(int i=0; serial && i< 32; i++) {
+        Serial.print(i<=ll ? "=":"-");
     }
+    if (serial) Serial.print("|");
+    for(int i=31; serial && i>-1; i--) {
+        Serial.print(i<=rr ? "=":"-");
+    }
+  
+    if(serial) Serial.print("] ");
+  
+    if(ll>rr+3) {
+      if (serial) Serial.print("Left");
+      motors(-255,255);
+    } else if(rr>ll+3) {
+      if (serial) Serial.print("Right");
+      motors(255,-255);
+    } else if (ll >4 && rr>4) {
+      if (serial) Serial.print("Fwd");
+      motors(255, 255);
+    } else {
+      if (serial) Serial.print("Search");
+      if((millis()%8000)>4000) {
+        motors(255,-255);
+      } else {
+        motors(-255,255);
+      }
+    }
+  
+    if (serial) Serial.println();
+    delay(7);
   }
+}
 
-  if (serial) Serial.println();
-  delay(7);
+void turn180DegreesToEscapeWhiteLine() {
+  if (rightLineSensorCounter <= (rightLineSensorCounterStart-rightLineSensorBack)) {
+      rotateLeft();
+  } else {
+      back();
+  }
+  //if (serial) Serial.printf("COUNTER: [%4d]", rightLineSensorCounter);
+  //if (serial) Serial.println("");
+  rightLineSensorCounter--;
+}
+
+void rotateLeft() {
+  motors(-255,255);
+}
+
+void back() {
+  motors(-255,-255);
 }
 
 int readLeft() {
-  //return analogRead(A1);
+  //return analogRead(A0);
   leftBuf = (leftBuf * LEN + analogRead(A1)) / (LEN+1);
   return leftBuf;
 }
 
 int readRight() {
-  //return analogRead(A0);
+  //return analogRead(A1);
   rightBuf = (rightBuf * LEN + analogRead(A0)) / (LEN+1);
   return rightBuf;
+}
+
+int readLineSensor(int sensor) {
+  return analogRead(sensor);
 }
