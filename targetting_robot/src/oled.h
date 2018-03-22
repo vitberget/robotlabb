c8ce5294740c74520a69c662b7f36e9d7a7ca8e5
Kenneth Hedman <github@vitberget.se>
2018-03-22 18:36:42 +0100

    Added examples

--- targetting_robot/src/oled.h
+++ targetting_robot/src/oled.h
@@ -7,4 +7,8 @@ void oled_begin();
 
 // Use 1-64 for left and right values
 // No rangecheck
+//
+// oled_show("Example1");
+// oled_show("Example2", 15, 43);
+// oled_show("Example2", 15, 43, 64, 12, 64, 23);
 void oled_show(String text, int left1=0, int right1=0, int left2=0, int right2=0, int left3=0, int right3=0);
