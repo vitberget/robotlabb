669ad9c9e57a14cb91670ab73d2a65c3cc86a6bf
Kenneth Hedman <github@vitberget.se>
2018-03-22 18:20:36 +0100

    Added instructions

--- targetting_robot/src/oled.h
+++ targetting_robot/src/oled.h
@@ -4,7 +4,6 @@
 
 void oled_begin();
 
-//void oled_show(String text);
-//void oled_show(String text, int left1, int right1);
-//void oled_show(String text, int left1, int right1, int left2, int right2);
+// Use 1-64 for left and right values
+// No rangecheck
 void oled_show(String text, int left1=0, int right1=0, int left2=0, int right2=0, int left3=0, int right3=0);
